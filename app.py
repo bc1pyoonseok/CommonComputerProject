c3f3cf5a6496c243d38c3aee7a9baac63eba4be2
ha_mulan <ham5312@naver.com>
2020-11-11 07:13:14 +0000

    error catch when no input

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    num=int(request.args.get('input',-1))
-    if num==-1:
+    try:
+        num=int(request.args.get('input'))
+        if num==1 or num==0:
+            return "{} has no primeFactor".format(num)
+        elif 0<num and num<=1000000:
+            return 'biggest primeFactor of {} is {}'.format(num,PrimeFactor(num))
+        else:
+            return "input range error input must be range of 0~1000000"
+    except:
         return 'NO INPUT!!! please give the input by adding  <  ?input={your input} > at the end of url'
-    if num==1:
-        return "1 has no primeFactor"
-    if num>1000000:
-        return "input range error"
-    return 'Hello your answer is {}!\n'.format(PrimeFactor(num))
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
